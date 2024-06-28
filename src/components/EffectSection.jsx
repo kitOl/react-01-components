2b51e2d8ffb139cf3c625b0541bb29ded5965594
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-06-28 05:20:49 +0300

    fix code for http

--- src/components/EffectSection.jsx
+++ src/components/EffectSection.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Button from "./Button/Button";
 import Modal from "./Modal/Modal";
 
@@ -7,17 +7,33 @@ export default function EffectSection() {
   const [loading, setLoading] = useState(false);
   const [users, setUsers] = useState([]);
 
-  async function fetchUsers() {
+  const fetchUsers = useCallback(async () => {
     setLoading(true);
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await response.json();
     setUsers(users);
     setLoading(false);
-  }
+  }, []);
+
+  // async function fetchUsers() {
+  //   setLoading(true);
+  //   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  //   const users = await response.json();
+  //   setUsers(users);
+  //   setLoading(false);
+  // }
 
   useEffect(() => {
+    // async function fetchUsers() {
+    //   setLoading(true);
+    //   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    //   const users = await response.json();
+    //   setUsers(users);
+    //   setLoading(false);
+    // }
+
     fetchUsers();
-  }, []);
+  }, [fetchUsers]);
 
   // function openModal() {
   // setModal(true);
